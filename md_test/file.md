9846c1f2315a949cdad42759f51a69de588af773
ShadowIsNotTim <t.musikov@yandex.ru>
2025-01-24 15:13:11 +0300

    add video to md_to_html

--- md_test/file.md
+++ md_test/file.md
@@ -4,3 +4,6 @@
 *bold!*
 *bold 2 hello!*
 [[img.jpg]]
+Видео файл:
+[[https://rutube.ru/video/785fd539adc52994aa5a41f251288f03]]
+[[https://youtu.be/VURUUmNXFi4?si=jOtR_kafuslSFuMv]]
--- md_to_html.py
+++ md_to_html.py
@@ -51,9 +51,20 @@ def markdown_to_html(md_text, css_path):
                     end_img = line.find(']]', start_img)
                     if end_img == -1:
                         break
-                    img_path = line[start_img + 2:end_img]
+                    link = line[start_img + 2:end_img].strip()
+                    el = ""
+                    if "rutube.ru" in link:
+                        link = link[link.rfind("/") + 1:]
+                        el = f'''<iframe width="720" height="405"
+                            src="https://rutube.ru/play/embed/{link}" frameBorder="0" allow="clipboard-write; autoplay"
+                            webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'''
+                    elif "youtu.be" in link:
+                        link = link[link.rfind("/") + 1:]
+                        el = f'''<iframe width="560" height="315" src="https://www.youtube.com/embed/{link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'''
+                    else:
+                        el = f'<img src="{link}" alt="Image">'
                     # img_path = os.path.join("files", img_path[img_path.rfind("/") + 1:])
-                    line = line[:start_img] + f'<img src="{img_path}" alt="Image">' + line[end_img + 2:]
+                    line = line[:start_img] + el + line[end_img + 2:]
                 html_text += f"<p>{line}</p>\n"
             else:
                 html_text += f"<p>{line}</p>\n"
