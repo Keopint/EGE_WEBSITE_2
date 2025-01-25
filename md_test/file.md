2eebd395e640b319860aebcb3b574c3964a83241
ShadowIsNotTim <t.musikov@yandex.ru>
2025-01-25 12:03:28 +0300

    add latex to md add requirements.txt

--- md_test/file.md
+++ md_test/file.md
@@ -8,3 +8,5 @@
 [[https://rutube.ru/video/785fd539adc52994aa5a41f251288f03]]
 [[https://youtu.be/VURUUmNXFi4?si=jOtR_kafuslSFuMv]]
 sdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfklsdlfjsdlkfjdsfkl
+
+Some complex formula: $$P(|S - E[S]| \ge t) \le 2 \exp \left( -\frac{2 t^2 n^2}{\sum_{i = 1}^n (b_i - a_i)^2} \right).$$
--- md_to_html.py
+++ md_to_html.py
@@ -1,7 +1,12 @@
 import os
+from mdtex2html import convert
 
 
 def markdown_to_html(md_text):
+    return links_converter(convert(md_text))
+
+
+def links_converter(md_text):
     html_text = ""
 
     lines = md_text.split('\n')
@@ -9,90 +14,30 @@ def markdown_to_html(md_text):
     table_lines = []
 
     for line in lines:
-        if is_table_separator(line):
-            continue
-        if line.strip().startswith('|') and line.strip().endswith('|'):
-            inside_table = True
-            table_lines.append(line)
+        if '[[' in line and ']]' in line:
+            while '[[' in line and ']]' in line:
+                start_img = line.find('[[')
+                end_img = line.find(']]', start_img)
+                if end_img == -1:
+                    break
+                link = line[start_img + 2:end_img].strip()
+                el = ""
+                if "rutube.ru" in link:
+                    link = link[link.rfind("/") + 1:]
+                    el = f'''<iframe width="720" height="405"
+                        src="https://rutube.ru/play/embed/{link}" frameBorder="0" allow="clipboard-write; autoplay"
+                        webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'''
+                elif "youtu.be" in link:
+                    link = link[link.rfind("/") + 1:]
+                    el = f'''<iframe width="560" height="315" src="https://www.youtube.com/embed/{link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'''
+                else:
+                    el = f'<img src="{link}" alt="Image">'
+                # img_path = os.path.join("files", img_path[img_path.rfind("/") + 1:])
+                line = line[:start_img] + el + line[end_img + 2:]
+            html_text += f"<p>{line}</p>\n"
         else:
-            if inside_table:
-                html_text += process_table(table_lines)
-                table_lines = []
-                inside_table = False
-
-            if line.startswith('#'):
-                header_level = 0
-                while line.startswith('#'):
-                    header_level += 1
-                    line = line[1:].strip()
-                html_text += f"<h{header_level}>{line}</h{header_level}>\n"
-            elif '*' in line:
-                while '*' in line:
-                    start_bold = line.find('*')
-                    end_bold = line.find('*', start_bold + 1)
-                    if end_bold == -1:
-                        break
-                    bold_text = line[start_bold + 1:end_bold]
-                    line = line[:start_bold] + f"<strong>{bold_text}</strong>" + line[end_bold + 2:]
-                html_text += f"<p>{line}</p>\n"
-            elif '[[' in line and ']]' in line:
-                while '[[' in line and ']]' in line:
-                    start_img = line.find('[[')
-                    end_img = line.find(']]', start_img)
-                    if end_img == -1:
-                        break
-                    link = line[start_img + 2:end_img].strip()
-                    el = ""
-                    if "rutube.ru" in link:
-                        link = link[link.rfind("/") + 1:]
-                        el = f'''<iframe width="720" height="405"
-                            src="https://rutube.ru/play/embed/{link}" frameBorder="0" allow="clipboard-write; autoplay"
-                            webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'''
-                    elif "youtu.be" in link:
-                        link = link[link.rfind("/") + 1:]
-                        el = f'''<iframe width="560" height="315" src="https://www.youtube.com/embed/{link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'''
-                    else:
-                        el = f'<img src="{link}" alt="Image">'
-                    # img_path = os.path.join("files", img_path[img_path.rfind("/") + 1:])
-                    line = line[:start_img] + el + line[end_img + 2:]
-                html_text += f"<p>{line}</p>\n"
-            else:
-                html_text += f"<p>{line}</p>\n"
-
-    if inside_table:
-        html_text += process_table(table_lines)
-    
+            html_text += f"<p>{line}</p>\n"
     return html_text
-
-def process_table(table_lines):
-    html = "<table>\n"
-    header_processed = False
-    
-    for line in table_lines:
-        line = line.strip('|').strip()
-        columns = [col.strip() for col in line.split('|')]
-
-        if not header_processed:
-            html += "  <thead>\n    <tr>\n"
-            for col in columns:
-                html += f"      <th>{col}</th>\n"
-            html += "    </tr>\n  </thead>\n  <tbody>\n"
-            header_processed = True
-        else:
-            html += "    <tr>\n"
-            for col in columns:
-                html += f"      <td>{col}</td>\n"
-            html += "    </tr>\n"
-    
-    html += "  </tbody>\n</table>\n"
-    return html
-
-def is_table_separator(line):
-    """
-    Check if a line is a table separator line consisting of pipes and dashes.
-    """
-    parts = line.strip().strip('|').split('|')
-    return all(set(part.strip()) <= {'-'} for part in parts)
 
 
 def main():
